6e04dbcf6cc76a088c8235ac5ac7d67e58493fb7
Roman Pelikh <pelikh.roman@gmail.com>
2019-07-11 13:01:13 -0300

    prepared front-end

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test name',
+      'A test description',
+      'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962997_960_720.jpg'
+    ),
+    new Recipe(
+      'Test name',
+      'A test description',
+      'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962997_960_720.jpg'
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
