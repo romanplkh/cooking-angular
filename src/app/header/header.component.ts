72c47de97cc3044858e82d3dbef452ee035de520
Roman Pelikh <pelikh.roman@gmail.com>
2019-09-06 00:02:28 -0300

    Changed add saveAll

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { DatabaseService } from '../shared/database.service';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { DatabaseService } from "../shared/database.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
+  selector: "app-header",
+  templateUrl: "./header.component.html",
   styleUrls: []
 })
 export class HeaderComponent implements OnInit {
   constructor(private db: DatabaseService) { }
 
-
   ngOnInit(): void { }
 
-
   onSaveData() {
-
+    this.db.saveRecipes().subscribe({
+      next: resp => {
+        console.log(resp);
+      },
+      error: err => console.log(err)
+    });
   }
 }
--- src/app/shared/database.service.ts
+++ src/app/shared/database.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RecepieService } from "../recipes/recipe.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DatabaseService {
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecepieService
+  ) { }
 
-
-
-
-
-
+  saveRecipes() {
+    //returns observable
+    return this.http.put(
+      "https://cooking-ang.firebaseio.com/recipes.json",
+      this.recipeService.getRecipes()
+    );
+  }
 }
